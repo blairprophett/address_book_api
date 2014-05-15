0787311459b8dcd75e0d729e6a50fab899bdaeeb
blairprophett <blaircuny@gmail.com>
2014-05-15 10:18:59 -0700

    emails working through console, not yet through app

--- email_sender/app/controllers/email_controller.rb
+++ email_sender/app/controllers/email_controller.rb
@@ -4,24 +4,15 @@ class EmailController < ApplicationController
   end
 
   def email
+    email = email_params
+    # binding.pry
+    SiteMailer.status_email(email[:email], email[:subject], email[:body]).deliver
 
-    
     render nothing: true, status: 200
-    # @email = Email.new(email_params)
-
-    # response_to do |format|
-    #   if @email.save
-    #     format.html{ redirect_to @email, notice: 'Email was successfully created.' }
-    #     format.json { render json: @email, status: :created}
-    #   else
-    #     format.html { render action: 'email' }
-    #     format.json { render json: @email.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
 private
- def movie_params
+ def email_params
     params.require(:email).permit(:subject, :body)
   end
 
